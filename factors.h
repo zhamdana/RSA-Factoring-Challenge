ebef310eef8b8e40095eddebbed493c5140adffb
Zaharadeen Hamdana <zaharadeenhamdana@gmail.com>
2023-01-30 18:22:11 +0000

    Remove

--- factors.h
+++
@@ -1,9 +0,0 @@
-#ifndef _FACTORS_H_
-#define _FACTORS_H_
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-#define _GNU_SOURCE
-/* size_t getline(char **lineptr, size_t *n, FILE *stream); */
-int factorize(char *buffer);
-#endif /* _FACTORS_H_ */
